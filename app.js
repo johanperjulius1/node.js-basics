6ec75f4ac83576045b46beea048329ea780d3ff6
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2021-04-26 16:26:10 +0200

    handling errors.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const https = require("https");
-const username = "chalkers";
 
 function printMessage(username, badgeCount, points) {
   const message = `${username} has ${badgeCount} total badge(s) and ${points} in JS.`;
@@ -7,19 +6,29 @@ function printMessage(username, badgeCount, points) {
   console.log(message);
 }
 
-const request = https.get(
-  `https://teamtreehouse.com/${username}.json`,
-  (response) => {
-    let body = ""
-    console.log(response.statusCode);
+function getProfile(username) {
+  const request = https.get(
+    `https://teamtreehouse.com/${username}.json`,
+    (response) => {
+      let body = "";
+      console.log(response.statusCode);
 
-    response.on("data", (data) => {
-      body += data.toString();
-    });
+      response.on("data", (data) => {
+        body += data.toString();
+      });
 
-    response.on('end', () => {
-      const profile = JSON.parse(body)
-      printMessage(username, profile.badges.length, profile.points.JavaScript)
-    })
-  }
-);
+      response.on("end", () => {
+        const profile = JSON.parse(body);
+        printMessage(
+          username,
+          profile.badges.length,
+          profile.points.JavaScript
+        );
+      });
+    }
+  );
+}
+
+const users = process.argv.slice(2);
+
+users.forEach(getProfile);
